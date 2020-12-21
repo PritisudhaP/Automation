e98189ca5195ad86e22576a80357bd85707dd561
sanjay-acuver <60770058+sanjay-acuver@users.noreply.github.com>
2020-12-21 19:31:51 +0530

    Added locators for Cancellation from new window

--- src/tests/screens/mixedOrder/mo_po.js
+++ src/tests/screens/mixedOrder/mo_po.js
@@ -27,8 +27,8 @@ module.exports = {
     	changePrice : element(by.xpath('(//*[text()[contains(.,"Change Price")]])[1]')),
     	priceTextBox : element(by.model('item.itemUnitPrice')),
     	savePrice : element(by.xpath('(//*[text()[contains(.,"Save")]])[3]')),
+    	item1PopUp : element(by.xpath('(//*[text()[contains(.,"BackOrdered")]])[1]')),
     	item2PopUp : element(by.xpath('(//*[text()[contains(.,"BackOrdered")]])[2]')),
-		item1PopUp : element(by.xpath('(//*[text()[contains(.,"BackOrdered")]])[1]')),
     	item3PopUp : element(by.xpath('(//*[text()[contains(.,"BackOrdered")]])[3]')),
     	fulfillmentType : element(by.model('product.item.fulfillmentType')),
     	fulfillmentStore : element(by.model('product.item.fulfillmentSite')),
@@ -49,7 +49,11 @@ module.exports = {
 	cancelSOLineLocators: {
 		//Cancel the SO line
 		cancelLine : element(by.xpath('(//*[text()[contains(.,"Cancel Line")]])[1]')),
-		confirmCancelBtn : element(by.xpath('//*[text()[contains(.,"Yes")]]')),
+		//confirmCancelBtn : element(by.xpath('//*[text()[contains(.,"Yes")]]')),
+    	cancelQty : element(by.xpath('(//*[@class="en-button"])[3]')),
+    	cancelReason : element(by.model('$parent.cancelReason')),
+    	confirmCancelBtn : element(by.xpath('//*[@class="ng-scope" and text()[contains(.,"Confirm")]]')),
+    	
 	},
 	
 	cancelFRLineLocators: {
@@ -81,6 +85,29 @@ module.exports = {
     	SONo : element(by.xpath('//*[text()[contains(.,"OMS Order #:")]]/following-sibling::strong')),
     	reshipSOBalance : element(by.xpath('//*[text()[contains(.,"Balance Due")]]/following-sibling::strong'))
     	
-	}
+	},
+	
+	invLookupLocators: { 
+		//For updating Scenario 6 Partial Release
+		menuInventory : element(by.xpath('(//*[text()[contains(.,"Inventory")]])[1]')),
+		tileInvLookup : element(by.xpath('(//div[@class="card card-bordered card-split card-lifted is-clickable"])[1]')),
+		addSkusBtn : element(by.xpath('//*[text()[contains(.,"Add SKUs")]]')),
+		skuSelectFilter : element(by.xpath('//*[text()[contains(.,"Filters")]]')),
+		skuSelectSearchCriteria : element(by.model('apiSearchFilter.filter')),
+		skuSelectSearchFilter : element(by.model('apiSearchFilter.arg')),
+		skuSearchTextBox : element(by.model('apiSearchFilter.value')),
+		skuDirectSearchTextBox : element(by.model('apiSearchText.value')),
+		selectSku : element(by.model('checked')),
+		skuSearchQty : element(by.model('item.orderQty')),
+		addSkuBtn : element(by.xpath('//*[text()[contains(.,"Add Skus")]]')),
+		searchBtn : element(by.xpath('//*[@class="button-primary en-button button-primary ng-binding" and text()[contains(.,"Search")]]')),
+		skuHamburgerBtn : element(by.xpath('//*[text()[contains(.,"AcuPartialSKU")]]/preceding::div[1]')),
+		skuDetails : element(by.xpath('//*[@class="ng-scope" and text()[contains(.,"Details")]]')),
+		entryAdjTab : element(by.xpath('//*[@class="en-tab" and text()[contains(.,"Entry Adjustment")]]')),
+		adjQtyBy : element(by.model('entry.data.amountToAdjust')),
+		adjReason : element(by.model('entry.data.description')),
+		saveBtn : element(by.xpath('//*[@class="ng-scope" and text()[contains(.,"Save")]]')),
+
+	 },
 };
 
